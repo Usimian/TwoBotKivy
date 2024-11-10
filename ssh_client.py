41f81536005950f93d83e26821bd4e756a3463f7
Usimian <marc@biopac.com>
2024-11-10 12:41:35 +0100

    slider working

--- ssh_client.py
+++ ssh_client.py
@@ -1,12 +1,9 @@
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
-from kivy.clock import Clock
-from kivy.uix.slider import Slider
-import socket
 import json
 import threading
+import socket
 from kivy.core.window import Window
+
+# from ssh_client_dashboard import set_K_values
 
 Window.size = (720, 480)
 
@@ -16,34 +13,15 @@ PORT = 5000  # The same port as the server
 
 class ssh_client():
     def __init__(self, **kwargs):
-        super(DataDisplay, self).__init__(**kwargs)
-        self.orientation = "vertical"
-
-        self.pos_slider = Slider(min=-100, max=100, value=0)
-        self.add_widget(Label(text="POSITION"))
-        self.add_widget(self.pos_slider)
-        self.pos_value = Label(text="0")
-        self.add_widget(self.pos_value)
-        self.pos_slider.bind(value=self.on_slider)
-
-        self.Kp = Label(text="Kp")
-        self.Ki = Label(text="Ki")
-        self.Kd = Label(text="Kd")
-
-        self.Kp2 = Label(text="Kp2")
-        self.Ki2 = Label(text="Ki2")
-        self.Kd2 = Label(text="Kd2")
-
-        self.add_widget(self.Kp)
-        self.add_widget(self.Ki)
-        self.add_widget(self.Kd)
-
-        self.add_widget(self.Kp2)
-        self.add_widget(self.Ki2)
-        self.add_widget(self.Kd2)
-
-        # self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # self.connect_to_server()
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connect_to_server()
+        self.Kp = 0
+        self.Ki = 0
+        self.Kd = 0
+        self.Kp2 = 0
+        self.Ki2 = 0
+        self.Kd2 = 0
+        self.json_data = {"Kp": 1, "Ki": 2, "Kd": 3, "Kp2": 4, "Ki2": 5, "Kd2": 6}
 
     def connect_to_server(self):
         try:
@@ -59,33 +37,17 @@ class ssh_client():
                 data = self.socket.recv(1024).decode()
                 if not data:
                     break
-                json_data = json.loads(data)
-                print(json_data)
-                Clock.schedule_once(lambda dt: self.update_labels(json_data))
+                self.json_data = json.loads(data)
+                self.data_rcv_update = True
+                self.Kp = self.json_data['Kp']
+                self.Ki = self.json_data['Ki']
+                self.Kd = self.json_data['Kd']
+                self.Kp2 = self.json_data['Kp2']
+                self.Ki2 = self.json_data['Ki2']
+                self.Kd2 = self.json_data['Kd2']
+                # print(self.json_data)
+                # Clock.schedule_once(lambda dt: self.update_labels(json_data))
             except Exception as e:
                 print(f"Error receiving data: {e}")
                 break
         self.socket.close()
-
-    def update_labels(self, data):
-        self.Kp.text = f"Proportional: {data['Kp']:.2f}"
-        self.Ki.text = f"Integral: {data['Ki']:.2f}"
-        self.Kd.text = f"Derivative: {data['Kd']:.2f}"
-        self.Kp2.text = f"Proportional 2: {data['Kp2']:.2f}"
-        self.Ki2.text = f"Integral 2: {data['Ki2']:.2f}"
-        self.Kd2.text = f"Derivative 2: {data['Kd2']:.2f}"
-
-    def on_slider(self, instance, val):
-        self.pos_value.val = int(val)
-        self.pos_value.text = f"{self.pos_value.val}"
-        response = self.pos_value.text
-        # self.socket.sendall(response.encode())      # Send position value to RP
-
-
-class TwoBotApp(App):
-    def build(self):
-        return DataDisplay()
-
-
-if __name__ == "__main__":
-    TwoBotApp().run()
--- ssh_client_dashboard.py
+++ ssh_client_dashboard.py
@@ -1,18 +1,24 @@
-from kivy.app import App
+from math import sin, cos
+import time
+import random
+# from ssh_client import ssh_client
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.slider import Slider
 from kivy_garden.graph import Graph, MeshLinePlot
 from kivy.clock import Clock
-from math import sin, cos
-import random
+from kivy.app import App
+
+from ssh_client import ssh_client
 
 
 class Dashboard(BoxLayout):
     def __init__(self, **kwargs):
         super(Dashboard, self).__init__(**kwargs)
         self.orientation = "vertical"
+
+        self.set_K_values(0, 0, 0, 0, 0, 0)
 
         # Upper half of the screen
         upper_half = BoxLayout(orientation="vertical", size_hint=(1, 0.5))
@@ -29,20 +35,19 @@ class Dashboard(BoxLayout):
         # Numerical dashboard items
         numbers_layout = GridLayout(cols=3, size_hint=(1, 0.8))
 
-        self.Kp = Label(text="Kp")
-        self.Ki = Label(text="Ki")
-        self.Kd = Label(text="Kd")
+        self.label_Kp = Label(text="Kp")
+        self.label_Ki = Label(text="Ki")
+        self.label_Kd = Label(text="Kd")
+        self.label_Kp2 = Label(text="Kp2")
+        self.label_Ki2 = Label(text="Ki2")
+        self.label_Kd2 = Label(text="Kd2")
 
-        self.Kp2 = Label(text="Kp2")
-        self.Ki2 = Label(text="Ki2")
-        self.Kd2 = Label(text="Kd2")
-
-        numbers_layout.add_widget(self.Kp)
-        numbers_layout.add_widget(self.Ki)
-        numbers_layout.add_widget(self.Kd)
-        numbers_layout.add_widget(self.Kp2)
-        numbers_layout.add_widget(self.Ki2)
-        numbers_layout.add_widget(self.Kd2)
+        numbers_layout.add_widget(self.label_Kp)
+        numbers_layout.add_widget(self.label_Ki)
+        numbers_layout.add_widget(self.label_Kd)
+        numbers_layout.add_widget(self.label_Kp2)
+        numbers_layout.add_widget(self.label_Ki2)
+        numbers_layout.add_widget(self.label_Kd2)
         upper_half.add_widget(numbers_layout)
 
         self.add_widget(upper_half)
@@ -69,29 +74,61 @@ class Dashboard(BoxLayout):
         self.graph.add_y_axis(ymin=0, ymax=100)
 
         self.plot1 = MeshLinePlot(color=[1, 0, 0, 1])
-        self.plot1.points = [(x, sin(x/4)*10) for x in range(0, 101)]
+        self.plot1.points = [(x, sin(x / 4) * 10) for x in range(0, 101)]
         self.graph.add_plot(self.plot1)
 
         self.plot2 = MeshLinePlot(color=[0, 1, 0, 1])
-        self.plot2.points = [(x, cos(x/4)*20) for x in range(0, 101)]
+        self.plot2.points = [(x, cos(x / 4) * 20) for x in range(0, 101)]
         self.graph.add_plot(self.plot2)
 
         self.add_widget(self.graph)
 
         # Start updating values
-        Clock.schedule_interval(self.update_values, 1)
+        # Clock.schedule_interval(self.update_values, 1)
 
+        self.client = ssh_client()       # Create ssh client return socket
+
+# Slider value changed
     def update_position(self, instance, value):
-        self.position_label.text = f"Position: {int(value)}"
+        self.position_label.text = f"{int(value)}"
+        self.position_label.value = int(value)
+        response = self.position_label.text
+        b_sent = self.client.socket.send(response.encode())      # Send position value to RP
+        # print(f"{b_sent}")
+        # print(f"{response}")
+        # time.sleep(0.2)
+
+    def set_K_values(self, Kp, Ki, Kd, Kp2, Ki2, Kd2):
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+        self.Kp2 = Kp2
+        self.Ki2 = Ki2
+        self.Kd2 = Kd2
 
     def update_values(self, dt):
-        value = round(random.uniform(0, 100), 2)
-        self.Kp = f"Kp: {value:.2f}"
+        # Update K values
+        self.Kp = self.client.json_data['Kp']
+        self.Ki = self.client.json_data['Ki']
+        self.Kd = self.client.json_data['Kd']
+        self.Kp2 = self.client.json_data['Kp2']
+        self.Ki2 = self.client.json_data['Ki2']
+        self.Kd2 = self.client.json_data['Kd2']
+        self.data_rcv_update = True
+
+        self.label_Kp.text = f"Kp: {self.Kp:.2f}"
+        self.label_Ki.text = f"Ki: {self.Ki:.2f}"
+        self.label_Kd.text = f"Kd: {self.Kd:.2f}"
+        self.label_Kp2.text = f"Kp2: {self.Kp2:.2f}"
+        self.label_Ki2.text = f"Ki2: {self.Ki2:.2f}"
+        self.label_Kd2.text = f"Kd2: {self.Kd2:.2f}"
+
+        # print("Update")
 
         # Update chart
-        # x_values = range(101)
-        # y_values = [random.uniform(0, 5) for _ in x_values]
-        # self.plot1.points = [(x, y) for x, y in zip(x_values, y_values)]
+        x_values = range(101)
+        y_values = [random.uniform(0, 5) for _ in x_values]
+        self.plot1.points = [(x, y) for x, y in zip(x_values, y_values)]
 
 
 class DashboardApp(App):
